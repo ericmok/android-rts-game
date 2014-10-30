6e294db08dbff4df3480abeef1f600a7e2c65f5e
ericmok <eric@mok.email>
2014-10-30 14:39:23 -0400

    Added entity class

---
+++ app/src/main/java/components/Entity.java
@@ -0,0 +1,15 @@
+package components;
+
+/**
+ * Created by eric on 10/30/14.
+ */
+public class Entity {
+
+    private int id;
+
+    public Entity() {
+        id = (int)(Math.random() * 1000);
+    }
+
+
+}
