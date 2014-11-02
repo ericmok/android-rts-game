aaa409c051e1bbd73c156c1102969ac1b03d99a4
ericmok <eric@mok.email>
2014-11-02 17:18:23 -0500

    Refactored and add reset to dest component

--- app/src/main/java/components/DestinationComponent.java
+++ app/src/main/java/components/DestinationComponent.java
@@ -6,5 +6,9 @@ import game.androidgame2.Vector2;
  * Created by eric on 11/2/14.
  */
 public class DestinationComponent extends Component {
-    public Vector2 destination = new Vector2();
+    public Vector2 dest = new Vector2();
+
+    public void reset() {
+        dest.zero();
+    }
 }
