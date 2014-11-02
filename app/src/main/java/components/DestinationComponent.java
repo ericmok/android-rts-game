fbb28b726c44b09b8e8ba4a7a67467f39f5ef047
ericmok <eric@mok.email>
2014-11-02 16:38:18 -0500

    Added destination component

---
+++ app/src/main/java/components/DestinationComponent.java
@@ -0,0 +1,10 @@
+package components;
+
+import game.androidgame2.Vector2;
+
+/**
+ * Created by eric on 11/2/14.
+ */
+public class DestinationComponent extends Component {
+    public Vector2 destination = new Vector2();
+}
