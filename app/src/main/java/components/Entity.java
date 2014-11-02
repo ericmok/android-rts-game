6656702481b76382cf305e134deab4f4c8e115ff
ericmok <eric@mok.email>
2014-11-02 17:59:20 -0500

    Removed followed tag since selection process keeps track of selected units

--- app/src/main/java/components/Entity.java
+++ app/src/main/java/components/Entity.java
@@ -28,9 +28,7 @@ public class Entity implements Denormalizable {
     public static final int TAG_FOLLOWER = 4;
 
     public static final int TAG_TROOP_TYPE = 5;
-
-    public static final int TAG_SELECTED = 6;
-
+    
     public Entity self = this;
 
     private static int NEXT_UNIQUE_ID = 1;
