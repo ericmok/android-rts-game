e14183362b7b09003bbec25d53e71d1a9b48523e
ericmok <eric@mok.email>
2014-11-03 14:32:55 -0500

    Added ability component

---
+++ app/src/main/java/components/AbilityComponent.java
@@ -0,0 +1,10 @@
+package components;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eric on 11/3/14.
+ */
+public class AbilityComponent extends Component {
+    public ArrayList<String> abilities = new ArrayList<String>(8);
+}
