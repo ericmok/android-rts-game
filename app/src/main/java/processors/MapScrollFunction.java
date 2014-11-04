e8c57345ef23ebb4b38b8039388c7d065c07b591
ericmok <eric@mok.email>
2014-11-03 22:06:27 -0500

    Added docstring

--- app/src/main/java/processors/MapScrollFunction.java
+++ app/src/main/java/processors/MapScrollFunction.java
@@ -7,6 +7,17 @@ import game.androidgame2.GameInput;
  * Created by eric on 11/3/14.
  */
 public class MapScrollFunction {
+
+    /**
+     * touchScrollDelta's are in screen coordinates. They are scaled up to larger world coordinates
+     * and mutate the game camera position which are also in world coordinates.
+     *
+     * The map is also multiplicatively scaled by gameInput's scaling value
+     *
+     * @param currentGesture
+     * @param gameInput
+     * @param gameCamera
+     */
     public static void process(int currentGesture, GameInput gameInput, GameCamera gameCamera) {
         if (currentGesture == GameInput.GESTURE_ON_SCROLL) {
             gameCamera.x += gameInput.touchScrollDeltas.x / gameCamera.scale;
