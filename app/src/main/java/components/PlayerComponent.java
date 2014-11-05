61c3de1051df866bbbe5b4f9a4cdbfb8dfba54dd
ericmok <eric@mok.email>
2014-11-05 16:56:46 -0500

    Added player component

---
+++ app/src/main/java/components/PlayerComponent.java
@@ -0,0 +1,11 @@
+package components;
+
+/**
+ * Created by eric on 11/5/14.
+ */
+public class PlayerComponent {
+
+    public static String ME = "me";
+
+    public String name = PlayerComponent.ME;
+}
