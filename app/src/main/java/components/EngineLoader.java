1e146de829414b9c1bb6d61ea646278a64ea3440
ericmok <eric@mok.email>
2014-11-06 15:54:09 -0500

    Engine loading delegated to separate class

---
+++ app/src/main/java/components/EngineLoader.java
@@ -0,0 +1,54 @@
+package components;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by eric on 11/6/14.
+ */
+public class EngineLoader {
+
+    public static boolean load(Engine engine, String mainPlayerName, String json) throws JSONException {
+
+        JSONObject jObj = new JSONObject(json);
+
+        engine.gameTime = jObj.getDouble("time");
+
+        JSONArray playersArr = jObj.getJSONArray("players");
+
+        for (int jp = 0; jp < playersArr.length(); jp++) {
+
+            JSONObject jPlayerObj = playersArr.getJSONObject(jp);
+
+            Player player = new Player(jPlayerObj.getString("name"));
+            player.team = jPlayerObj.getInt("team");
+
+            engine.addPlayer(player);
+
+            if (player.name.equals(mainPlayerName)) {
+                engine.currentPlayer = player;
+            } else {
+                // Allied or enemy team
+            }
+
+            JSONArray troopArr = jPlayerObj.getJSONArray("troop");
+
+            for (int i = 0; i < troopArr.length(); i++) {
+                JSONObject jEntity = troopArr.getJSONObject(i);
+
+                Entity troop = GameEntities.buildTroop();
+
+                WorldComponent pc =
+                        ((WorldComponent) troop.cData.get(WorldComponent.class));
+                pc.set(jEntity.getDouble("x"), jEntity.getDouble("y"));
+
+                player.denorms.addDenormalizable(troop);
+            }
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/game/androidgame2/LevelLoader.java
+++ app/src/main/java/game/androidgame2/LevelLoader.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 
+import components.EngineLoader;
 import components.Entity;
 import components.GameEntities;
 import components.WorldComponent;
@@ -38,45 +39,11 @@ public class LevelLoader {
 
             String json = new String(buffer, "UTF-8");
             Log.i("LEVEL JSON", json);
+
             try {
-                JSONObject jObj = new JSONObject(json);
-
-                engine.gameTime = jObj.getDouble("time");
-
-                JSONArray playersArr = jObj.getJSONArray("players");
-
-                for (int jp = 0; jp < playersArr.length(); jp++) {
-
-                    JSONObject jPlayerObj = playersArr.getJSONObject(jp);
-
-                    Player player = new Player(jPlayerObj.getString("name"));
-                    player.team = jPlayerObj.getInt("team");
-
-                    engine.addPlayer(player);
-
-                    if (player.name.equals("self")) {
-                        engine.currentPlayer = player;
-                    } else {
-                        // Allied or enemy team
-                    }
-
-                    JSONArray troopArr = jPlayerObj.getJSONArray("troop");
-
-                    for (int i = 0; i < troopArr.length(); i++) {
-                        JSONObject jEntity = troopArr.getJSONObject(i);
-
-                        Entity troop = GameEntities.buildTroop();
-
-                        WorldComponent pc =
-                                ((WorldComponent) troop.cData.get(WorldComponent.class));
-                        pc.set(jEntity.getDouble("x"), jEntity.getDouble("y"));
-
-                        player.denorms.addDenormalizable(troop);
-                    }
-                }
+                EngineLoader.load(engine, "self", json);
             }
             catch (JSONException e) {
-                Log.e("JSON", e.getMessage());
                 e.printStackTrace();
             }
 
