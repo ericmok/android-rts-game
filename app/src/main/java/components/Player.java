6cebf99525e82c3c34d8f5a285ee72204a01abee
ericmok <eric@mok.email>
2014-11-06 14:37:41 -0500

    Add team colors to player

--- app/src/main/java/components/Player.java
+++ app/src/main/java/components/Player.java
@@ -1,11 +1,19 @@
 package components;
 
+import android.graphics.Color;
+
 import java.util.ArrayList;
 
 /**
  * Created by eric on 11/5/14.
  */
 public class Player {
+    public static ArrayList<Integer> TeamColors = new ArrayList<Integer>() {{
+       this.add(Color.WHITE);
+       this.add(Color.RED);
+       this.add(Color.YELLOW);
+    }};
+
     public String name = "player_" + Math.floor(Math.random() * 10000);
     public int team = 0;
     public DenormalizedDataSet<Entity, Integer> denorms = new DenormalizedDataSet<Entity, Integer>(32, 300);
@@ -16,5 +24,13 @@ public class Player {
 
     public boolean equals(Player otherPlayer) {
         return this.name == otherPlayer.name;
+    }
+
+    public int color() {
+        return colorForTeam(team);
+    }
+
+    public static int colorForTeam(int team) {
+        return TeamColors.get(team);
     }
 }
