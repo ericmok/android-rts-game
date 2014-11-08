6a460307ec90b48483d55ca81987167db31b5cc6
ericmok <eric@mok.email>
2014-11-08 00:40:13 -0500

    Added TroopSpec constants

---
+++ app/src/main/java/processors/TroopSpec.java
@@ -0,0 +1,13 @@
+package processors;
+
+/**
+ * Created by eric on 11/7/14.
+ */
+public class TroopSpec {
+    public static double RADIUS = 1;
+
+    public static double ATTACK_RANGE = 2;
+
+    public static double MAX_MOVE_SPEED = 1.0;
+    public static double ACCELERATION = 0.1;
+}
