8b6de04aea2fe219500e785bde89f0e7aaf5a3eb
ericmok <eric@mok.email>
2014-11-08 16:57:19 -0500

    Made battleresolution take account of rotation

--- app/src/main/java/processors/BattleResolution.java
+++ app/src/main/java/processors/BattleResolution.java
@@ -7,11 +7,14 @@ import model.Entity;
 import model.LivingComponent;
 import model.Player;
 import model.WorldComponent;
+import utils.Vector2;
 
 /**
  * Created by eric on 11/7/14.
  */
 public class BattleResolution {
+
+    private static Vector2 temp = new Vector2();
 
     public static void process(Engine engine, double dt) {
 
@@ -34,13 +37,23 @@ public class BattleResolution {
                         WorldComponent wc2 = (WorldComponent)troops2.get(t).cData.get(WorldComponent.class);
 
                         if (wc1.pos.distanceTo(wc2.pos) < 2) {
-                            LivingComponent lc1 = (LivingComponent)troops1.get(s).cData.get(LivingComponent.class);
-                            LivingComponent lc2 = (LivingComponent)troops2.get(t).cData.get(LivingComponent.class);
-                            lc1.hitPoints = 0;
-                            lc2.hitPoints = 0;
+                            Vector2.subtract(temp, wc2.pos, wc1.pos);
+                            temp.setNormalized();
 
-                            player1.queueRemoved(troops1.get(s));
-                            player2.queueRemoved(troops2.get(t));
+                            if (wc1.rot.dotProduct(temp) > 0.4) {
+                                LivingComponent lc2 = (LivingComponent)troops2.get(t).cData.get(LivingComponent.class);
+                                lc2.hitPoints = 0;
+                                player2.queueRemoved(troops2.get(t));
+                            }
+
+                            temp.set(-temp.x, -temp.y);
+
+                            if (wc2.rot.dotProduct(temp) > 0.4) {
+                                LivingComponent lc1 = (LivingComponent) troops1.get(s).cData.get(LivingComponent.class);
+                                lc1.hitPoints = 0;
+                                player1.queueRemoved(troops1.get(s));
+                            }
+
                         }
                     }
                 }
