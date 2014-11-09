d19d1e5c53c3d3c6a1e0fdc2f81be5ea77c6d9d1
ericmok <eric@mok.email>
2014-11-09 14:16:30 -0500

    Fixed comments

--- app/src/main/java/model/GameEntities.java
+++ app/src/main/java/model/GameEntities.java
@@ -8,52 +8,11 @@ import utils.MemoryPool;
  */
 public class GameEntities {
 
+    /**
+     * Already scaffolded troops you can fetch and use.
+     */
     public static MemoryPool<Troop> troopsMemoryPool = new MemoryPool<Troop>(Troop.class, 1024);
 
-//    /**
-//     * Construct an entity representing a troop
-//     * @return
-//     */
-//    public static Entity buildTroop() {
-//
-//        //Entity troop = new Entity();
-//        Entity troop = troopsMemoryPool.fetchMemory();
-//        troop.labels().add(Entity.UNIT_TROOP);
-////
-////        PlayerComponent pc = new PlayerComponent();
-////        pc.name = "default";
-////        pc.team = team;
-////        troop.cData.put(PlayerComponent.class, pc);
-//
-//        WorldComponent worldComponent = new WorldComponent();
-//        troop.cData.put(WorldComponent.class, worldComponent);
-//
-//        SelectionComponent selectionComponent = new SelectionComponent();
-//        troop.cData.put(SelectionComponent.class, selectionComponent);
-//        troop.labels().add(Entity.NODE_SELECTION);
-//
-//        DestinationComponent destinationComponent = new DestinationComponent();
-//        troop.cData.put(DestinationComponent.class, destinationComponent);
-//        troop.labels().add(Entity.NODE_MOVE_TOWARD_DESTINATION);
-//
-//        troop.labels().add(Entity.NODE_TROOP_DRAWER);
-//
-//        AbilityComponent ac = new AbilityComponent();
-//        ac.abilities.add(Abilities.SPECIAL_ATTACK);
-//        troop.cData.put(AbilityComponent.class, ac);
-//
-//        LivingComponent lc = new LivingComponent();
-//        troop.cData.put(LivingComponent.class, lc);
-////
-////        troop.labels().add(team);
-////        troop.labels().add(leader);
-//
-//        return troop;
-//    }
-
-//    public static void recycleTroop(Entity toRecycle) {
-//        troopsMemoryPool.recycleMemory(toRecycle);
-//    }
 
     public static void recycle(Entity entity) {
         if (entity.labels().contains(Entity.UNIT_TROOP)) {
