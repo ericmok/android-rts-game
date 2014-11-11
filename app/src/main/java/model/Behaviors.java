e4ae6458f0f3eccfca928e28929ad4a5228453da
ericmok <eric@mok.email>
2014-11-11 13:43:34 -0500

    Spaces

--- app/src/main/java/model/Behaviors.java
+++ app/src/main/java/model/Behaviors.java
@@ -9,8 +9,10 @@ public class Behaviors {
     public static final int BEHAVIOR_DRAWN_AS_TROOP = 5;
     public static final int BEHAVIOR_MOVES_TOWARD_DESTINATION = 8;
     public static final int BEHAVIOR_CASTS_ABILITIES = 9;
+
     public static final int BEHAVIOR_LEADER = 103;
     public static final int BEHAVIOR_FOLLOWER = 104;
+    
     public static final int UNIT_TROOP = 105;
     public static final int UNIT_BASIC_PROJECTILE = 106;
     public static final int UI_BUTTON = 500;
