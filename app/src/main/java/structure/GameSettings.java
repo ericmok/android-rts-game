7be0ce1e30d8eb5576eee66ef42f7f95a90b2ea1
ericmok <eric@mok.email>
2014-11-13 20:50:11 -0500

    Increased game speed

--- app/src/main/java/structure/GameSettings.java
+++ app/src/main/java/structure/GameSettings.java
@@ -5,5 +5,5 @@ package structure;
  */
 public class GameSettings {
     public static final float UNIT_LENGTH_MULTIPLIER = 0.07f;
-    public static final double UNIT_TIME_MULTIPLIER = 0.0007;
+    public static final double UNIT_TIME_MULTIPLIER = 0.0008;
 }
