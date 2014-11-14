446561f84f24418490390f6a298a9cf66e528360
ericmok <eric@mok.email>
2014-11-14 12:47:05 -0500

    Added velocity component to projectile

--- app/src/main/java/model/Projectile.java
+++ app/src/main/java/model/Projectile.java
@@ -11,6 +11,9 @@ public class Projectile extends Entity {
         WorldComponent worldComponent = new WorldComponent();
         this.cData.put(WorldComponent.class, worldComponent);
 
+        VelocityComponent vc = new VelocityComponent();
+        this.cData.put(VelocityComponent.class, vc);
+
         this.labels().add(Behaviors.BEHAVIOR_MOVES_TOWARD_DESTINATION);
         DestinationComponent destinationComponent = new DestinationComponent();
         this.cData.put(DestinationComponent.class, destinationComponent);
