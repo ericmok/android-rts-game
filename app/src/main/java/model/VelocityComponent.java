3383f186bdcd0a3fef1dacf599b3c5378b56ab27
ericmok <eric@mok.email>
2014-11-15 21:59:03 -0500

    Added acceleration to velcoity component

--- app/src/main/java/model/VelocityComponent.java
+++ app/src/main/java/model/VelocityComponent.java
@@ -11,4 +11,6 @@ public class VelocityComponent extends Component {
 
     public Vector2 velocity = new Vector2();
     public double deltaDegrees = 0;
+
+    public Vector2 acceleration = new Vector2();
 }
