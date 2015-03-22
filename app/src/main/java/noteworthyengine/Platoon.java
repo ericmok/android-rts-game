26c61483b2db83e74a7025fff7a4dfb6a9176d97
ericmok <eric@mok.email>
2015-03-22 14:46:22 -0400

    Change attack swing animation to a proper sprite with dynamic positioning

--- app/src/main/java/noteworthyengine/Platoon.java
+++ app/src/main/java/noteworthyengine/Platoon.java
@@ -6,6 +6,7 @@ import noteworthyframework.Gamer;
 import noteworthyframework.Unit;
 import structure.Sprite2dDef;
 import structure.TemporarySprite2dDef;
+import utils.Orientation;
 import utils.VoidFunc;
 import utils.VoidFunc2;
 import utils.VoidFunc3;
@@ -132,28 +133,44 @@ public class Platoon extends Unit {
                 system.drawCompat.tempSpritesMemoryPool.recycleMemory(tempSprite);
             }
 
-            if (onAttackSwingAnim) {
-                TemporarySprite2dDef tempSprite = system.drawCompat.tempSpritesMemoryPool.fetchMemory();
-                //tempSprite.position.x = (battleNode.coords.pos.x + battleNode.coords.rot.x * battleNode.attackRange.v / 2);
-                //tempSprite.position.y = (battleNode.coords.pos.y + battleNode.coords.rot.y * battleNode.attackRange.v / 2);
-                tempSprite.position.x = (battleNode.coords.pos.x + target[0].coords.pos.x) / 2;
-                tempSprite.position.y = (battleNode.coords.pos.y + target[0].coords.pos.y) / 2;
+            if (battleNode.attackState.v == BattleNode.ATTACK_STATE_SWINGING) {
+                Sprite2dDef tempSprite = system.drawCompat.spriteAllocator.takeNextWritable();
+                double rx = (target[0].coords.pos.x - battleNode.coords.pos.x);
+                double ry =(target[0].coords.pos.y - battleNode.coords.pos.y);
+                tempSprite.position.x = (0.45 * (battleNode.attackProgress.v / battleNode.attackSwingTime.v) + 0.15) * rx  + battleNode.coords.pos.x;
+                tempSprite.position.y = (0.45 * (battleNode.attackProgress.v / battleNode.attackSwingTime.v) + 0.15) * ry  + battleNode.coords.pos.y;
                 tempSprite.position.z = 1;
-                tempSprite.width = 0.3f;
-                tempSprite.height = 0.3f;
-                tempSprite.angle = (float)movementNode.coords.rot.getDegrees();
-                tempSprite.progress.progress = 1;
-                tempSprite.progress.duration = (float)battleNode.attackSwingTime.v * 1000; // should be 1000 but...
+                tempSprite.width = 0.39f;
+                tempSprite.height = 0.39f;
+                tempSprite.angle = (float) Orientation.getDegreesBaseX(rx, ry);
                 tempSprite.isGfxInterpolated = false;
                 tempSprite.animationName = Sprite2dDef.ANIMATION_TROOPS_PROJECTILE;
                 tempSprite.animationProgress = 0;
                 tempSprite.color = Color.argb(120, 255, 255, 255);
-
-                system.drawCompat.drawTemporarySprite(tempSprite);
-                system.drawCompat.tempSpritesMemoryPool.recycleMemory(tempSprite);
-
-                onAttackSwingAnim = false;
             }
+
+//            if (onAttackSwingAnim) {
+//                TemporarySprite2dDef tempSprite = system.drawCompat.tempSpritesMemoryPool.fetchMemory();
+//                //tempSprite.position.x = (battleNode.coords.pos.x + battleNode.coords.rot.x * battleNode.attackRange.v / 2);
+//                //tempSprite.position.y = (battleNode.coords.pos.y + battleNode.coords.rot.y * battleNode.attackRange.v / 2);
+//                tempSprite.position.x = (battleNode.coords.pos.x + target[0].coords.pos.x) / 2;
+//                tempSprite.position.y = (battleNode.coords.pos.y + target[0].coords.pos.y) / 2;
+//                tempSprite.position.z = 1;
+//                tempSprite.width = 0.3f;
+//                tempSprite.height = 0.3f;
+//                tempSprite.angle = (float)movementNode.coords.rot.getDegrees();
+//                tempSprite.progress.progress = 1;
+//                tempSprite.progress.duration = (float)battleNode.attackSwingTime.v * 1000; // should be 1000 but...
+//                tempSprite.isGfxInterpolated = false;
+//                tempSprite.animationName = Sprite2dDef.ANIMATION_TROOPS_PROJECTILE;
+//                tempSprite.animationProgress = 0;
+//                tempSprite.color = Color.argb(120, 255, 255, 255);
+//
+//                system.drawCompat.drawTemporarySprite(tempSprite);
+//                system.drawCompat.tempSpritesMemoryPool.recycleMemory(tempSprite);
+//
+//                onAttackSwingAnim = false;
+//            }
 
             //dyingRenderNode.isActive = true;
         }
