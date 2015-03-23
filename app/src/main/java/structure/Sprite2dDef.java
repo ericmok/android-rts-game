9a0fbde2340743d2ba1e7b26cb0197f58aa7d594
ericmok <eric@mok.email>
2015-03-23 01:02:34 -0400

    Add constant

--- app/src/main/java/structure/Sprite2dDef.java
+++ app/src/main/java/structure/Sprite2dDef.java
@@ -13,6 +13,9 @@ import utils.Vector3;
  */
 public class Sprite2dDef {
 
+    public static final String ANIMATION_SPLASH_SPLASH = "Animations/Splash/Splash";
+    public static final String ANIMATION_SPLASH_LOADER = "Animations/Splash/Loader";
+
     public static final String ANIMATION_SMOKE_GUNPOWDER = "Animations/Smoke/Gunpowder";
 	public static final String ANIMATION_TROOPS_IDLING = "Animations/Troops/Idling";
 	public static final String ANIMATION_TROOPS_MOVING = "Animations/Troops/Moving";
