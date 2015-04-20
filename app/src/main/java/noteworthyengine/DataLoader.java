25885727b59f047d435a6373a8a78b755cbc8db0
ericmok <eric@mok.email>
2015-04-19 20:51:08 -0400

    Remove platoonleader in level

--- app/src/main/java/noteworthyengine/DataLoader.java
+++ app/src/main/java/noteworthyengine/DataLoader.java
@@ -67,15 +67,15 @@ public class DataLoader implements EngineDataLoader {
 
                 baseEngine.addUnit(troopy);
 
-                if (i < 2) {
-                    PlatoonLeader platoonLeader = new PlatoonLeader();
-                    platoonLeader.formationLeader.gamer.v = player;
-                    platoonLeader.formationLeader.coords.pos.copy(coords.pos);
-                    baseEngine.addUnit(platoonLeader);
-                }
+//                if (i < 2) {
+//                    PlatoonLeader platoonLeader = new PlatoonLeader();
+//                    platoonLeader.formationLeader.gamer.v = player;
+//                    platoonLeader.formationLeader.coords.pos.copy(coords.pos);
+//                    baseEngine.addUnit(platoonLeader);
+//                }
 
 
-                for (int k = 0; k < 0; k++) {
+                for (int k = 0; k < 3; k++) {
                     //Unit troop = GameEntities.troopsMemoryPool.fetchMemory();
                     if (player.name.equals("evilempire")) {
                         troopy = new Mech(player);
