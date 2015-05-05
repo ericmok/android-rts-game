af906b30c6355ad541c4d6b0aa924e5e7a6ba004
ericmok <eric@mok.email>
2015-05-05 13:46:29 -0400

    Add level selector

--- app/src/main/java/noteworthyengine/screens/LevelSelector.java
+++ app/src/main/java/noteworthyengine/screens/LevelSelector.java
@@ -1,7 +1,30 @@
 package noteworthyengine.screens;
 
+import noteworthyengine.InputSystem;
+import noteworthyengine.RenderSystem;
+import noteworthyframework.BaseEngine;
+import structure.Game;
+import structure.GameCamera;
+
 /**
  * Created by eric on 5/4/15.
  */
-public class LevelSelector {
+public class LevelSelector extends BaseEngine {
+
+    public final Game game;
+
+    public final GameCamera gameCamera;
+
+    public InputSystem inputSystem;
+    public RenderSystem renderSystem;
+
+    public LevelSelector(Game game) {
+        this.game = game;
+        this.gameCamera = this.game.getGameRenderer().registerNewOrthoCamera();
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
 }
