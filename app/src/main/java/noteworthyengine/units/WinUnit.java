e1371b94cb128f5432b819bf6cd950717eff0b8b
ericmok <eric@mok.email>
2015-05-20 17:05:43 -0400

    Update win defeat effects

--- app/src/main/java/noteworthyengine/units/WinUnit.java
+++ app/src/main/java/noteworthyengine/units/WinUnit.java
@@ -8,6 +8,7 @@ import noteworthyengine.InputNode;
 import noteworthyengine.InputSystem;
 import noteworthyengine.RenderNode;
 import noteworthyengine.events.GameEvents;
+import noteworthyframework.BaseEngine;
 import noteworthyframework.Unit;
 import utils.Vector2;
 
@@ -19,6 +20,7 @@ public class WinUnit extends Unit {
     public static final String NAME = "winUnit";
 
     public InputNode inputNode = new SwitcherInput(this);
+    private double animationProgress = 0;
 
 //    public InputNode inputNode = new InputNode(this) {
 //        @Override
@@ -36,7 +38,7 @@ public class WinUnit extends Unit {
         super();
         this.name = NAME;
 
-        renderNode.set(0, 0, 0, 2, 2, 0, Color.WHITE, Animations.ANIMATIONS_WIN_DEFEAT_WIN, 0, 1);
+        renderNode.set(0, 0.5f, 0, 1.2f, 1.2f, 0, Color.WHITE, Animations.ANIMATIONS_WIN_DEFEAT_WIN, 0, 1);
     }
 
     public static class SwitcherInput extends InputNode {
@@ -51,5 +53,13 @@ public class WinUnit extends Unit {
             // TODO: Defeat signal
             inputSystem.getBaseEngine().emitEvent(GameEvents.QUIT_WIN);
         }
+    }
+
+    @Override
+    public void step(BaseEngine engine, double dt) {
+        super.step(engine, dt);
+        animationProgress += dt;
+
+        renderNode.coords.pos.x += 0.0005 * Math.sin(0.2 * animationProgress);
     }
 }
