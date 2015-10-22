b059cbc7d64a995f6bfde7b99801462fc8f3c9a9
ericmok <eric@mok.email>
2015-10-21 22:30:12 -0400

    Make Mine use new BattleNode

--- app/src/main/java/noteworthyengine/units/Mine.java
+++ app/src/main/java/noteworthyengine/units/Mine.java
@@ -1,5 +1,7 @@
 package noteworthyengine.units;
 
+import android.graphics.Color;
+
 import art.Animations;
 import noteworthyengine.BattleNode;
 import noteworthyengine.BattleSystem;
@@ -27,7 +29,7 @@ public class Mine extends Unit {
     public GridNode gridNode;
     public MovementNode movementNode = new MovementNode(this);
     public SeparationNode separationNode = new SeparationNode(this);
-    public BattleNode battleNode = new BattleNode(this);
+    public BattleNode battleNode = new MineBattleNode(this);
     public RenderNode renderNode = new RenderNode(this);
 
     public RewriteOnlyArray<BattleNode.Target> battleTargets =
@@ -44,26 +46,20 @@ public class Mine extends Unit {
         renderNode.onDraw = new VoidFunc<RenderSystem>() {
             @Override
             public void apply(RenderSystem system) {
-                if (!battleNode.isAlive()) {
-                    //TemporarySprite2dDef tempSprite = system.defineNewTempSprite(Animations.ANIMATION_TROOPS_DYING_DEF, 0);
-                    //system.drawCompat.tempSpritesMemoryPool.fetchMemory();
 
+                if (!battleNode.isAlive()) {
                     TemporarySprite2dDef tempSprite = system.beginNewTempSprite();
 
                     tempSprite.copy(Animations.ANIMATION_TROOPS_DYING_DEF);
-                    tempSprite.setPosition((float) battleNode.coords.pos.x, (float) battleNode.coords.pos.y, 1);
+                    tempSprite.setPosition((float) renderNode.coords.pos.x, (float) renderNode.coords.pos.y, 0);
 
                     system.endNewTempSprite(tempSprite, 0);
-
-                    //system.drawCompat.drawTemporarySprite(tempSprite);
-                    //system.drawCompat.tempSpritesMemoryPool.recycleMemory(tempSprite);
                 }
 
                 if (battleNode.attackState.v == BattleNode.ATTACK_STATE_SWINGING) {
                     float ratio = (float)(battleNode.attackProgress.v / battleNode.attackSwingTime.v);
                     float rad = (float)(battleNode.attackRange.v * ratio);
 
-                    //Sprite2dDef sprite2dDef = system.drawCompat.spriteAllocator.takeNextWritable();
                     Sprite2dDef sprite2dDef = system.defineNewSprite(Animations.ANIMATION_MINE_EXPLODING,
                             (int) (ratio * 100),
                             (float)battleNode.coords.pos.x,
@@ -74,21 +70,6 @@ public class Mine extends Unit {
                             0,
                             renderNode.color.v,
                             0);
-//                    sprite2dDef.isGfxInterpolated = false;
-//                    sprite2dDef.animationName = Animations.ANIMATION_MINE_EXPLODING;
-//                    sprite2dDef.animationProgress = (int) (ratio * 100);
-//                    sprite2dDef.position.x = battleNode.coords.pos.x;
-//                    sprite2dDef.position.y = battleNode.coords.pos.y;
-//                    sprite2dDef.position.z = 3;
-//                    sprite2dDef.angle = 0;
-//                    sprite2dDef.width = 2 * rad;
-//                    sprite2dDef.height = 2 * rad;
-//                    sprite2dDef.color = renderNode.color.v;
-
-                    //renderNode.animationName = Sprite2dDef.ANIMATION_MINE_EXPLODING;
-                    //renderNode.width.v = 2 * rad;
-                    //renderNode.height.v = 2 * rad;
-                    //renderNode.animationProgress.v = (int) (ratio * 100);
                 }
 //                else {
 //                    renderNode.animationName = Sprite2dDef.ANIMATION_MINE_IDLING;
@@ -109,9 +90,22 @@ public class Mine extends Unit {
         }
 
         @Override
-        public void onAttackReady(BattleSystem battleSystem, BattleNode target) {
-            super.onAttackReady(battleSystem, target);
-            battleSystem.getBaseEngine().removeUnit(this.unit);
+        public void reset() {
+            super.reset();
+            this.nonCancellableSwing.v = 0;
+            this.lockOnAttack.v = 0;
+            this.fractionToWalkIntoAttackRange.v = 0.2;
+            this.targetAcquisitionRange.v = 8;
+            this.attackRange.v = 1.9;
+            this.attackDamage.v = 50;
+            this.attackSwingTime.v = 1.5;
+            this.attackCooldown.v = 3;
+            this.startingHp.v = 400;
+            this.hp.v = 400;
+            this.isAttackable.v = 1;
+            this.attackState.v = BattleNode.ATTACK_STATE_READY;
+            this.target.v = null;
+
         }
 
         @Override
@@ -121,36 +115,26 @@ public class Mine extends Unit {
 
             for (int j = mine.battleTargets.size() - 1; j >= 0; j--) {
                 BattleNode toInflict = mine.battleTargets.get(j).v;
-                toInflict.inflictDamage(battleSystem, mine.battleNode, mine.battleNode.attackDamage.v);
+                toInflict.inflictDamage(battleSystem, this, this.attackDamage.v);
             }
         }
     }
 
     public void reset() {
 
-        movementNode.maxSpeed.v = 0.1;
+        movementNode.maxSpeed.v = 0.9;
 
-        battleNode.nonCancellableSwing.v = 0;
-        battleNode.fractionToWalkIntoAttackRange.v = 0.3;
-        battleNode.targetAcquisitionRange.v = 8;
-        battleNode.attackRange.v = 2.5;
-        battleNode.attackDamage.v = 40;
-        battleNode.attackSwingTime.v = 3.5;
-        battleNode.attackCooldown.v = 3;
-        battleNode.hp.v = 100;
-        battleNode.isAttackable.v = 1;
-        battleNode.attackState.v = BattleNode.ATTACK_STATE_READY;
-        battleNode.target.v = null;
+        battleNode.reset();
 
         renderNode.animationName.v = Animations.ANIMATION_MINE_IDLING;
         renderNode.isGfxInterpolated.v = 0;
-        renderNode.width.v = 0.9f;
-        renderNode.height.v = 0.9f;
+        renderNode.width.v = 0.95f;
+        renderNode.height.v = 0.95f;
         renderNode.z.v = 2;
     }
 
     public void configure(Gamer gamer) {
         battleNode.gamer.v = gamer;
-        renderNode.color.v = Gamer.TeamColors.get(gamer.team) & 0x80ffffff;
+        renderNode.color.v = Gamer.TeamColors.get(gamer.team) & 0xaaffffff;
     }
 }
